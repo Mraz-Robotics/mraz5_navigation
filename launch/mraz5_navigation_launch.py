49cc2a199a096af8892730bc097dd0924d1e3d00
SimonMacoMrazRoboticsxD <simon.maco@mrazrobotics.com>
2025-03-14 13:25:54 +0100

    modified:   config/nav2_params.yaml 	modified:   launch/mraz5_navigation_launch.py 	new file:   map/map.pgm 	new file:   map/map.yaml

--- launch/mraz5_navigation_launch.py
+++ launch/mraz5_navigation_launch.py
@@ -1,36 +1,72 @@
-import os.path
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from nav2_common.launch import RewrittenYaml
+import os
 
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    mr5_localization_dir = get_package_share_directory('mraz5_navigation')
-    
-    config_param_nav2 = os.path.join(mr5_localization_dir,'config', 'nav2_params.yaml')
-    
-    param_substitutions = {
-        'global_frame': LaunchConfiguration('global_frame', default='odom')}
-    configured_params = RewrittenYaml(
-            source_file=config_param_nav2,
-            root_key='',
-            param_rewrites=param_substitutions,
-            convert_types=True)
-    
+    robotdir = get_package_share_directory('mraz5_navigation')
+    navdir = get_package_share_directory('nav2_bringup')
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    use_rviz = LaunchConfiguration('use_rviz', default='true')
+    map_dir = LaunchConfiguration(
+        'map',
+        default=os.path.join(
+            robotdir,
+            'map',
+            'map.yaml'))
 
-    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
-    nav2_bringup_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_nav2_bringup,
-            'launch/navigation_launch.py')),
-        launch_arguments={'use_sim_time': 'False', 'params_file': configured_params}.items(),
-    )
-    
-    ld = LaunchDescription()
-    ld.add_action(nav2_bringup_launch)
+    param_file_name = 'nav2_params.yaml'
+    config_dir = LaunchConfiguration(
+        'config',
+        default=os.path.join(
+            robotdir,
+            'config',
+            param_file_name))
 
-    return ld
+    nav2_launch_file_dir = os.path.join(navdir, 'launch')
+
+    rviz_config_dir = os.path.join(
+        navdir,
+        'rviz',
+        'nav2_default_view.rviz')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map',
+            default_value=map_dir,
+            description='Full path to map file to load'),
+
+        DeclareLaunchArgument(
+            'config',
+            default_value=config_dir,
+            description='Full path to param file to load'),
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([nav2_launch_file_dir, '/bringup_launch.py']),
+            launch_arguments={
+                'map': map_dir,
+                'use_sim_time': use_sim_time,
+                'params_file': config_dir}.items(),
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time': use_sim_time}],
+            condition=IfCondition(use_rviz),
+            output='screen'),
+    ])
